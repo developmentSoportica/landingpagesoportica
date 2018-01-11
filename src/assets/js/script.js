dcc5bc88ee88265cf20bd768a6b7ab189bf312fd
developmentSoportica <developer@soportica.com.co>
2018-01-11 15:25:51 -0500

    second commit in the app

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -126,16 +126,16 @@ var Tawk_API = Tawk_API || {},
 // consume api of backend{
 
 
-const privateKey = '',
-    publicKey = '',
-    content = document.getElementById('content');
+// const privateKey = '',
+//     publicKey = '',
+//     content = document.getElementById('content');
 
-    const getConnection = () =>{
-        const ts = Date.now(),
-            URL = '';
-            fetch(URL).then(response => console.log(response));
-    }
+//     const getConnection = () =>{
+//         const ts = Date.now(),
+//             URL = '';
+//             fetch(URL).then(response => console.log(response));
+//     }
 
-    getConnection();
+//     getConnection();
 
 
